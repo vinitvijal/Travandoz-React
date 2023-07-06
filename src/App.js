7a06dee3ffd3a27a89e19230f62c7d64b4009440
Vineet <vineet.22566@sscbs.du.ac.in>
2023-07-06 13:24:59 +0530

    Under Development Header Applied

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './components/Home';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import Development from './components/Development';
 // I want to check whether the user is logged in or not and show the appropriate page
 function App() {
   return (
-    <Routes>
+  <>
+  <Development/>
+  <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
+  </>
+    
   );
 }
 
---
+++ src/components/Development.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Development() {
+  return (
+    <div style={{'display':'flex', 'height':'8vh', 'color':'#3CA689', 'align-items':'center','justify-content':'center','background':'#f0f0f0','font-size':'1.4rem'}}>Under Development</div>
+  )
+}
